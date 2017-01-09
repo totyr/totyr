477225e4d3af47e45f6f7f788e7b9b9009b7560f
totyr <liulongchn@gmail.com>
2017-01-09 16:12:05 +0800

    Update oracle.sql

--- notes/db/oracle/oracle.sql
+++ notes/db/oracle/oracle.sql
@@ -1,3 +1,8 @@
 -- 字符串转行
 select regexp_substr('a,b,c','[^,]+', 1, level) from dual
 connect by regexp_substr(a,b,c', '[^,]+', 1, level) is not null
+--递归写法
+select e.id,e.pid,e.name,level
+from table e
+start with  e.pid is null
+connect by prior e.id = e.pid;
