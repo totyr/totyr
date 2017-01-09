5829da6c71ea0489134dcbbd915c9a0e2b7ca59f
totyr <liulongchn@gmail.com>
2017-01-09 16:17:25 +0800

    Update oracle.sql

--- notes/db/oracle/oracle.sql
+++ notes/db/oracle/oracle.sql
@@ -1,8 +1,13 @@
 -- 字符串转行
 select regexp_substr('a,b,c','[^,]+', 1, level) from dual
-connect by regexp_substr(a,b,c', '[^,]+', 1, level) is not null
+connect by regexp_substr(a,b,c', '[^,]+', 1, level) is not null;
+                         
 --递归写法
-select e.id,e.pid,e.name,level
-from table e
-start with  e.pid is null
-connect by prior e.id = e.pid;
+select id,name,pid
+,level
+,sys_connect_by_path(name, '/') name_tree
+,connect_by_root id as root
+,connect_by_isleaf as isleaf
+from table t
+start with  t.pid is null
+connect by pid = prior id;
