2cb949a507367c98fd407288fab5d47cd6870440
totyr <liulongchn@gmail.com>
2017-02-10 14:13:53 +0800

    Update ddl.sql

--- notes/db/ddl.sql
+++ notes/db/ddl.sql
@@ -1,2 +1,5 @@
 --添加唯一约束oracle
-ALTER TABLE table_name ADD CONSTRAINT constraint_name UNIQUE(column_name);
+ALTER TABLE "table_name" ADD CONSTRAINT "constraint_name" UNIQUE(column_name);
+
+--删除非空约束pg
+alter table "table_name" alter column "column_name"  drop  not null;
