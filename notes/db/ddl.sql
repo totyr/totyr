c7a4d95c13dd430de90bcfdd53374c7bcbe1ce3e
totyr <liulongchn@gmail.com>
2017-02-14 14:03:30 +0800

    Update ddl.sql

--- notes/db/ddl.sql
+++ notes/db/ddl.sql
@@ -3,3 +3,9 @@ ALTER TABLE "table_name" ADD CONSTRAINT "constraint_name" UNIQUE(column_name);
 
 --删除非空约束pg
 alter table "table_name" alter column "column_name"  drop  not null;
+
+
+--注释
+--添加注释oracle
+comment on table "table_name" is '';
+comment on column "table_name"."column_name" is '';
