05d5c46659110fd81f9aba3f4250c6b55b7cc84a
totyr <liulongchn@gmail.com>
2017-03-08 14:01:21 +0800

    Update ddl.sql

--- notes/db/ddl.sql
+++ notes/db/ddl.sql
@@ -1,14 +1,14 @@
 --添加唯一约束oracle
-ALTER TABLE "table_name" ADD CONSTRAINT "constraint_name" UNIQUE(column_name);
+ALTER TABLE <table_name> ADD CONSTRAINT <constraint_name> UNIQUE(column_name);
 
 --删除非空约束pg
-alter table "table_name" alter column "column_name"  drop  not null;
+alter table <table_name> alter column <column_name>  drop  not null;
 
 
 --注释
 --添加注释oracle
-comment on table "table_name" is '';
-comment on column "table_name"."column_name" is '';
+comment on table <table_name> is '';
+comment on column <table_name>.<column_name> is '';
 
---禁用日志oracle
-alter table "table_name" nologging;
+--启用/禁用日志oracle
+alter table <table_name> logging/nologging;
