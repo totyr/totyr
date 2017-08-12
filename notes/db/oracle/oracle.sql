bf791c9e1ae27969b2a138e46715317ccb73da73
totyr <liulongchn@gmail.com>
2017-08-12 11:04:59 +0800

    Update oracle.sql

--- notes/db/oracle/oracle.sql
+++ notes/db/oracle/oracle.sql
@@ -11,3 +11,20 @@ select id,name,pid
 from table t
 start with  t.pid is null
 connect by pid = prior id;
+                         
+--递归示例：查到当前部门及子部门
+ with a (org_code,sub_code)as(
+         select org_code as org_code,org_code as sub_code
+         from dbo.org_info
+         --where--start with
+         union all
+         select a.org_code,b.org_code
+         from dbo.org_info b,a
+         where b.pid=a.sub_code
+  )
+ select * from a;
+                         
+--遍历写法
+for i in(select job_name from(select 'a' as job_name from dual)a) loop
+    DBMS_OUTPUT.PUT_LINE(i.job_name);
+end loop;
