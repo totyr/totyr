7081e385c52af10fe1e80935787cd62310c968c5
totyr <liulongchn@gmail.com>
2017-08-26 09:34:16 +0800

    Update jqgrid_ext.js

--- notes/js/jqgrid_ext.js
+++ notes/js/jqgrid_ext.js
@@ -1,3 +1,12 @@
 var tyUtilsJqGrid={
-
+   //初始化参数   
+   params:{
+        rowNum:10000,//默认显示行数为20,如果不做分页，需要修改
+      },
+  //单元格格式化方法
+  colModelTextFormatter:function(cellvalue, options, rowObj){
+  },
+   //单元格反格式化方法(获取单元格内容时可通过jqgrid getrowdata而不是人工解析)
+  colModelTextFormatter:function(cellvalue, options, rowObj){
+  },
 }
